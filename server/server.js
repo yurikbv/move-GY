4f597b09ab83b9ec8dfad6f57d2bbd50197fee67
Yuriy Bochkarev <yurikbvv@gmail.com>
2020-12-05 19:27:24 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -44,13 +44,13 @@ app.use( (req, res, next) => {
 } )
 
 // Default route for production
-if(process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static('client/build/'))
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
-  })
-}
+
+const path = require('path');
+app.use(express.static('client/build/'))
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+})
+
 
 const PORT = process.env.PORT;
 
