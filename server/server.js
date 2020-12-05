9de7085582cd136646f69de9bf5951eb55c63298
Yuriy Bochkarev <yurikbvv@gmail.com>
2020-12-05 19:36:41 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -48,11 +48,11 @@ app.use( (req, res, next) => {
 const path = require('path');
 app.use(express.static('client/build/'))
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
