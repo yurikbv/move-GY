bb3ff7d65ad49cfcec9b854a573663b9897b01fd
Yuriy Bochkarev <yurikbvv@gmail.com>
2020-12-05 22:20:32 +0300

    deploy

--- client/src/helpers/misc.js
+++ client/src/helpers/misc.js
@@ -1,4 +1,4 @@
-export const REACT_APP_API_URL = 'https://move-gy-2.herokuapp.com/api';
+export const REACT_APP_API_URL = '/api/';
 export const REACT_APP_FACEBOOK_CLIENT = '391517745607051';
 export const REACT_APP_GOOGLE_CLIENT = 
 '304687935308-3lbjffogbg3rt5ks09k13jgeib81r6qt.apps.googleusercontent.com';
