e4ecadb68e4b2de42691d9b0cb34c4aef4143bf4
yurikbv <yurikbvv@gmail.com>
2021-01-24 22:18:00 +0300

    update

--- client/src/helpers/misc.js
+++ client/src/helpers/misc.js
@@ -1,3 +1,4 @@
+// export const REACT_APP_API_URL = 'http://localhost:5000/api';
 export const REACT_APP_API_URL = '/api';
 export const REACT_APP_FACEBOOK_CLIENT = '770247870367817';
 export const REACT_APP_GOOGLE_CLIENT = '';
